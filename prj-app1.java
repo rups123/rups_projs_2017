064eb07fb4c4f45fd26f875189ca1c08fde146d6
rups123 <32500944+rups123@users.noreply.github.com>
2017-10-04 20:47:21 -0700

    Create prj-app1.java

---
+++ prj-app1.java
@@ -0,0 +1,10 @@
+package 2017.rups.prj.app1
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello World");
+    }
+
+}
